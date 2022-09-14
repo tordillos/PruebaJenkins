d434240f5f5e59fb1efea9e34d996f7aea8f1a22
INDRA\pblazquez <pblazquez@minsait.com>
2022-09-14 15:20:08 +0200

    Subida ejemplo

---
+++ src/main/java/com/minsait/pedroblazquez/PruebaServlet.java
@@ -0,0 +1,32 @@
+package com.minsait.pedroblazquez;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class PruebaServlet
+ */
+public class PruebaServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public PruebaServlet() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter()
+		.append("Servidor iniciando ")
+		.append("Esto es una llamada GET");
+	}
+	
+}
---
+++ target/basico.maven/index.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+<h1>Ejemplo Curso Minsait</h1>
+<h2>Pedro BH Ejemplo War</h2>
+</body>
+</html> 
