e4b5bf271acaadc73dc77983d1c4065a8044f1f9
INDRA\pblazquez <pblazquez@minsait.com>
2022-09-14 17:06:18 +0200

    Se añaden Test

---
+++ src/test/java/com/minsait/pedroblazquez/ExampleTest.java
@@ -0,0 +1,14 @@
+package com.minsait.pedroblazquez;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ExampleTest {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
